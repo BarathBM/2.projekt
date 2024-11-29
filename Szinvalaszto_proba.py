65dda79f08522275042698b59c2572f9211cf984
BarathBM <bercimiki1@gmail.com>
2024-11-29 08:42:38 +0100

    Clean

--- Szinvalaszto_proba.py
+++ Szinvalaszto_proba.py
@@ -1,6 +1,10 @@
-import tkinter as tk
-
-root=tk.Tk()
+import tkinter as Tk
+import random
+import tkinter.font as tkFont
+root=Tk.Tk()
 root.title("Színválasztós")
+cimsor=Tk.Label(root, text="Színválasztó")
+myfont=tkFont.Font()
+
 
 root.mainloop()
