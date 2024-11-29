a5f4a6b677d2684c983da2653ee29647b351c31e
OregTuz00001 <kanyaimark2007@gmail.com>
2024-11-29 08:42:13 +0100

    szinek

--- Szinvalaszto_proba.py
+++ Szinvalaszto_proba.py
@@ -3,4 +3,18 @@ import tkinter as tk
 root=tk.Tk()
 root.title("Színválasztós")
 
+
+
+
+szinek=['kék', 'piros', 'zöld', 'sárga', 'narancs', 'rózsaszín', 'lila']
+szinek_tömb={
+    'kék':'blue',
+    'piros':'red',
+    'zöld':'green',
+    'sárga':'yellow',
+    'narancs':'orange',
+    'rózsaszín':'pink',
+    'lila':'purple'
+    }
+
 root.mainloop()
